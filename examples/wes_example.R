40a3b4eed6ad20b14368c25064b368f2ddaa1e26
James Scott <james.scott@mccombs.utexas.edu>
2016-07-01 18:24:29 +0100

    wes's example

---
+++ examples/wes_example.R
@@ -0,0 +1,21 @@
+library(FDRreg)
+
+# Data
+z_mat = read.csv('../data/0.csv', header=FALSE)
+z = as.numeric(as.matrix(z_mat))
+
+hist(z, 100, prob=TRUE)
+curve(dnorm(x, 0, 3), add=TRUE, n=1000, col='red')
+
+# Deconvolution under theoretical null
+out1 = prfdr(z, control=list(npasses=25, gridsize=250))
+plot(out1$x_grid, out1$pitheta_grid/(1-out1$pi0), type='l')
+
+# Empirical null
+f0 = efron(z, nulltype='empirical')
+mu0 = f0$mu0
+sig0 = f0$sig0
+
+# Deconvolution under empirical null
+out2 = prfdr(z, mu0, sig0, control=list(npasses=25, gridsize=250))
+plot(out2$x_grid, out2$pitheta_grid/(1-out2$pi0), type='l')
