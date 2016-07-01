8025d1a2bad5e4723fa246a69b11174b14e5da18
James Scott <james.scott@mccombs.utexas.edu>
2016-07-01 18:38:58 +0100

    wes example

--- examples/wes_example.R
+++ examples/wes_example.R
@@ -8,7 +8,7 @@ hist(z, 100, prob=TRUE)
 curve(dnorm(x, 0, 3), add=TRUE, n=1000, col='red')
 
 # Deconvolution under theoretical null
-out1 = prfdr(z, control=list(npasses=25, gridsize=250))
+out1 = prfdr(z, control=list(npasses=25, gridsize=500))
 plot(out1$x_grid, out1$pitheta_grid/(1-out1$pi0), type='l')
 
 # Empirical null
@@ -19,3 +19,8 @@ sig0 = f0$sig0
 # Deconvolution under empirical null
 out2 = prfdr(z, mu0, sig0, control=list(npasses=25, gridsize=250))
 plot(out2$x_grid, out2$pitheta_grid/(1-out2$pi0), type='l')
+
+
+# Compare convolution of estimate for pi(theta) with true f1
+plot(out1$x_grid, out1$f1_grid)
+curve(dnorm(x, 0, sqrt(9+1)), add=TRUE, col='red')
